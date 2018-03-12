c30080c7e4ecb036d30965af7b2ab3ba44d7be49
jsales3 <jsales3>
2018-03-11 23:32:50 -0400

    first commit

---
+++ TravelReview/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReview.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Display (Name = "Location")]
+        public string CategoryName { get; set; }
+        [Display(Name = "Location Type")]
+        public string CategoryType { get; set; }
+
+        //Navingation Property
+        public virtual ICollection<Review> Reviews { get; set; }
+
+    }
+}
---
+++ TravelReview/Models/Review.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TravelReview.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [Display(Name = "Review")]
+        public string Comments { get; set; }
+        [Display(Name = "Rating")]
+        public int Ratings { get; set; }
+
+       
+
+
+
+        //Foreign Key Navigation Properties
+        [ForeignKey("Category")]
+        [Display(Name = "Location")]
+        public int CategoryID { get; set; }
+        [Display(Name = "Location Type")]
+        public virtual Category Category { get; set; }
+        
+
+
+    }
+}
